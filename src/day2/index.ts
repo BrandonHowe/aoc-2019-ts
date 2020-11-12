881f9c4a731c3c1e5f4ed5627942f6d948e066be
xWafl <73sampleperson@gmail.com>
2020-11-12 14:43:23 -0500

    feat: finished day 2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,12 +1,57 @@
 import * as path from "path";
-import { readInputSplitNum } from "../helpers/readInput";
+import { readInputRaw } from "../helpers/readInput";
+
+const parseIntcode = (raw: number[]) => {
+    const program = [...raw];
+    let currentPointer = 0;
+    while (true) {
+        const val = program[currentPointer];
+        if (val === 1) {
+            program[program[currentPointer + 3]] =
+                program[program[currentPointer + 1]] +
+                program[program[currentPointer + 2]];
+        } else if (val === 2) {
+            program[program[currentPointer + 3]] =
+                program[program[currentPointer + 1]] *
+                program[program[currentPointer + 2]];
+        } else if (val === 99) {
+            return program;
+        }
+        currentPointer += 4;
+    }
+};
+
+const findValue = (raw: number[], target: number) => {
+    for (let i = 0; i < 100; i++) {
+        for (let j = 0; j < 100; j++) {
+            const copiedRaw = [...raw];
+            copiedRaw[1] = i;
+            copiedRaw[2] = j;
+            if (parseIntcode(copiedRaw)[0] === target) {
+                return 100 * i + j;
+            }
+        }
+    }
+};
 
 const main = async () => {
-    const input = await readInputSplitNum(path.join(__dirname, "./input.txt"));
+    const input = await readInputRaw(path.join(__dirname, "./input.txt"));
 
-    console.time("main");
+    const program = input.split(",").map(Number);
 
-    console.timeEnd("main");
+    console.time("part1");
+
+    const result = parseIntcode(program);
+
+    console.log("Part 1:", result[0]);
+
+    console.timeEnd("part1");
+
+    console.time("part2");
+
+    console.log("Part 2:", findValue(program, 19690720));
+
+    console.timeEnd("part2");
 };
 
 main();
