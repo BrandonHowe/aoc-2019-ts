ec545827ba59c3b164b9aa4dc323e5e9b350698b
xWafl <73sampleperson@gmail.com>
2020-11-13 15:14:33 -0500

    feat: finished day 4

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -1,12 +1,60 @@
 import * as path from "path";
-import { readInputSplitNum } from "../helpers/readInput";
+import { readInputRaw, readInputSplitNum } from "../helpers/readInput";
+
+const passwordMatches = (pass: number) => {
+    const strPass = pass.toString();
+    if (strPass.length !== 6) return;
+    let doubleFound = false;
+    for (let i = 0; i < strPass.length - 1; i++) {
+        if (Number(strPass.charAt(i)) > Number(strPass.charAt(i + 1))) {
+            return false;
+        }
+        if (Number(strPass.charAt(i)) === Number(strPass.charAt(i + 1))) {
+            doubleFound = true;
+        }
+    }
+    return doubleFound;
+};
+
+const passwordMatchesP2 = (pass: number) => {
+    const strPass = pass.toString();
+    if (strPass.length !== 6) return;
+    let doubleFound = false;
+    for (let i = 0; i < strPass.length - 1; i++) {
+        if (Number(strPass.charAt(i)) > Number(strPass.charAt(i + 1))) {
+            return false;
+        }
+        if (
+            Number(strPass.charAt(i - 1)) !== Number(strPass.charAt(i)) &&
+            Number(strPass.charAt(i)) === Number(strPass.charAt(i + 1)) &&
+            Number(strPass.charAt(i + 2)) !== Number(strPass.charAt(i))
+        ) {
+            doubleFound = true;
+        }
+    }
+    return doubleFound;
+};
 
 const main = async () => {
-    const input = await readInputSplitNum(path.join(__dirname, "./input.txt"));
+    const input = (await readInputRaw(path.join(__dirname, "./input.txt")))
+        .split("-")
+        .map(Number);
 
-    console.time("main");
+    console.time("part1");
 
-    console.timeEnd("main");
+    const pwArr = Array(input[1] - input[0] + 1)
+        .fill(null)
+        .map((_, idx) => idx + input[0]);
+
+    console.log("Part 1:", pwArr.filter(passwordMatches).length);
+
+    console.timeEnd("part1");
+
+    console.time("part2");
+
+    console.log("Part 2:", pwArr.filter(passwordMatchesP2).length);
+
+    console.timeEnd("part2");
 };
 
 main();
