a52c318906134465314ffdd32a0cbf68bb821ead
xWafl <73sampleperson@gmail.com>
2020-11-14 07:07:06 -0500

    feat: finished day 6

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -1,12 +1,58 @@
 import * as path from "path";
-import { readInputSplitNum } from "../helpers/readInput";
+import { readInputSplit } from "../helpers/readInput";
+
+const findOrbitCount = (
+    orbits: Record<string, string>,
+    planet: string
+): string[] => {
+    return planet in orbits
+        ? [...findOrbitCount(orbits, orbits[planet]), planet]
+        : ["COM"];
+};
 
 const main = async () => {
-    const input = await readInputSplitNum(path.join(__dirname, "./input.txt"));
+    const input: Record<string, string> = (
+        await readInputSplit(path.join(__dirname, "./input.txt"))
+    )
+        .map(l => l.split(")"))
+        .reduce(
+            (acc, cur) => ({ ...acc, [cur[1]]: cur[0] }),
+            {} as Record<string, string>
+        );
 
-    console.time("main");
+    console.time("part1");
 
-    console.timeEnd("main");
+    const paths = Object.keys(input).reduce(
+        (acc, cur) => ({
+            ...acc,
+            [cur]: findOrbitCount(input, cur).slice(0, -1)
+        }),
+        {} as Record<string, string[]>
+    );
+
+    console.log(
+        "Part 1:",
+        Object.values(paths).reduce((acc, cur) => acc + cur.length, 0)
+    );
+
+    console.timeEnd("part1");
+
+    console.time("part2");
+
+    const youPath = paths.YOU;
+    const sanPath = paths.SAN;
+
+    const intersection = youPath.filter(l => sanPath.includes(l));
+
+    console.log(
+        "Part 2:",
+        youPath.length -
+            intersection.length +
+            sanPath.length -
+            intersection.length
+    );
+
+    console.timeEnd("part2");
 };
 
 main();
