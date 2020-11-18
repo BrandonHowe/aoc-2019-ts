dc5e887b2cfa324c9ff6dd407f0681eb94d91599
xWafl <73sampleperson@gmail.com>
2020-11-18 14:19:23 -0500

    feat: finished day 8

--- src/day8/index.ts
+++ src/day8/index.ts
@@ -1,12 +1,64 @@
 import * as path from "path";
-import { readInputSplitNum } from "../helpers/readInput";
+import { displayGridObjConfig } from "../helpers/displayGrid";
+import { readInputRaw } from "../helpers/readInput";
 
 const main = async () => {
-    const input = await readInputSplitNum(path.join(__dirname, "./input.txt"));
+    const input = (await readInputRaw(path.join(__dirname, "./input.txt")))
+        .split("")
+        .filter(l => l !== "\n")
+        .map(Number);
 
-    console.time("main");
+    const width = 25;
+    const height = 6;
 
-    console.timeEnd("main");
+    console.time("part1");
+
+    const board: number[][][] = [];
+
+    for (let i = 0; i < input.length / (width * height); i++) {
+        const newLayer = [];
+        for (let j = 0; j < height; j++) {
+            const row = [];
+            for (let l = 0; l < width; l++) {
+                row.push(input[i * width * height + j * width + l]);
+            }
+            newLayer.push(row);
+        }
+        board.push(newLayer);
+    }
+
+    const zeroCounts = board.map(l => l.flat().filter(l => l === 0).length);
+    const matchingLayer = board[zeroCounts.indexOf(Math.min(...zeroCounts))];
+    const total =
+        matchingLayer.flat().filter(l => l === 1).length *
+        matchingLayer.flat().filter(l => l === 2).length;
+
+    console.log("Part 1:", total);
+
+    console.timeEnd("part1");
+
+    console.time("part2");
+
+    const newBoard: number[][] = Array(height)
+        .fill(42)
+        .map(() => Array(width).fill(2));
+
+    for (let i = 0; i < board.length; i++) {
+        const boardRows = board[i].length;
+        for (let j = 0; j < boardRows; j++) {
+            for (let k = 0; k < board[i][j].length; k++) {
+                if (newBoard[j][k] === 2) {
+                    newBoard[j][k] = board[i][j][k];
+                }
+            }
+        }
+    }
+
+    console.log(
+        "Part 2:\n" + displayGridObjConfig(newBoard, { 1: "#", 0: " " })
+    );
+
+    console.timeEnd("part2");
 };
 
 main();
